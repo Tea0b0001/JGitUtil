1892bc5d82ebbc52d13c49549ce3a841207c66b5
Tea0b0001 <guanxian_he@163.com>
2019-11-27 10:44:30 +0800

    git commit test

--- src/DoJGit.java
+++ src/DoJGit.java
@@ -6,7 +6,7 @@
 public class DoJGit {
     public static void main(String... args) {
         //your code
-        System.out.println("dododododo");
+        System.out.println("dododnew");
     }
 
 }
